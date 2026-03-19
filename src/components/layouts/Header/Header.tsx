f52fea4c4f1f07ef3bf51cc8e8dd37b6b4f48b70
Ted <jjboy830227@gmail.com>
2026-03-19 18:25:47 +0800

    feat: add logo and update navigation structure in Header component for better responsiveness

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -16,11 +16,14 @@ export const Header: React.FC = () => {
   return (
     <header className="fixed top-0 left-0 z-50 bg-background/85 backdrop-blur-md h-16 w-full border-b-2 border-gray-100 px-10">
       <div className="h-full flex gap-10">
+        {/* LOGO */}
         <Link href="/" className="flex h-full items-center gap-1">
           <Image src="/ted_logo.png" alt="logo" width={50} height={50} />
           <h1 className="text-2xl font-bold">{title}</h1>
         </Link>
-        <nav className="flex h-full items-center gap-6">
+
+        {/* Desktop Nav */}
+        <nav className="hidden md:flex h-full items-center gap-6">
           <NavLink href="/about">關於我</NavLink>
           <NavLink href="/portfolio">作品集</NavLink>
           <NavLink href="/contact">聯絡我</NavLink>
